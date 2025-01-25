04274a5eb8293f8a049d47132b7dcc3897445be0
hzml <hongzhe2022@163.com>
2025-01-25 18:10:45 +0800

    fix:start.py

--- start.py
+++ start.py
@@ -1,8 +1,11 @@
 import subprocess
+import os
+
+os.chdir("./src")  # 修改当前工作目录
 
 # 设置 Python 解释器路径和脚本路径
-python_exe = ".\\python-embed\\pythonw.exe"
-script_path = ".\\src\\main.py"
+python_exe = "..\python-embed\python.exe"
+script_path = "main.py"
 
 # 使用 subprocess 非阻塞执行，隐藏 DOS 窗口
 process = subprocess.Popen([python_exe, script_path], creationflags=subprocess.CREATE_NO_WINDOW)
